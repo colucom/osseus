c9f1e893193e60b886c878a4fa46585cb4131ac9
Lior Rabin <lior@colu.com>
2018-04-10 11:36:01 +0300

    some updates

--- index.js
+++ index.js
@@ -1,44 +1,46 @@
-const auto = require('async').auto;
+const auto = require('async').auto
+const modules = {}
 
 const init = async (config) => {
   return new Promise(async (resolve, reject) => {
-    console.log('0')
     config = config || await require('osseus-config').init()
-    console.log('1')
 
     let osseus = {
       config: config
     }
 
-    let mudules = initModulDeps(osseus)
-    console.log('3')
+    initModuleDeps(osseus)
 
     auto(modules, (err) => {
-      console.log('4')
       if (err) return reject(err)
       resolve(osseus)
     })
   })
 }
 
-const initModulDeps = (osseus) => {
-  modules = {}
-  console.log('2', osseus.config.keys)
+const initModuleDeps = (osseus) => {
+  console.log(`osseus.config.keys: ${osseus.config.keys}`)
   osseus.config.keys.forEach(async (key) => {
-    console.log('key', key)
+    console.log(`key: ${key}`)
     if (key.startsWith('osseus')) {
-      let mudoleName = key.substring(7) // 'osseus_' length
-      modules[mudoleName] = osseus.config[key].dependencies || []
-      modules[mudoleName].push(async () => {
-        console.log('require(' + key.replace('_', '-') + ').init()...')
-        let mudole = await require(key.replace('_', '-')).init(osseus)
-        console.log('required', key.replace('_', '-'))
-        if (mudole.start) {
-          console.log(key.replace('_', '-') + '.start()...')
-          await mudole.start()
-          console.log('started', key.replace('_', '-'))
+      let moduleName = key.substring(7) // 'osseus_' length
+      let dependencies = osseus.config[key].dependencies || []
+      if (typeof dependencies === 'string') {
+        dependencies = dependencies.split(',')
+      }
+      console.log(`dependencies: ${dependencies}`)
+      modules[moduleName] = dependencies
+      modules[moduleName].push(async () => {
+        key = key.replace('_', '-')
+        console.log(`require(${key}).init()...`)
+        let module = await require(key).init(osseus)
+        console.log(`required: ${key}`)
+        if (module.start) {
+          console.log(`${key}.start()...`)
+          await module.start()
+          console.log(`started ${key}`)
         }
-        osseus[mudoleName] = mudole
+        osseus[moduleName] = module
         return module
       })
     }
