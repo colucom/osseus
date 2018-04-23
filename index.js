0be2ee7f2c97619091ceb63899ad50ff246dc5d0
Lior Rabin <lior@colu.com>
2018-04-23 12:37:39 +0300

    console.log if debug

--- index.js
+++ index.js
@@ -19,26 +19,31 @@ const init = async (config) => {
 }
 
 const initModuleDeps = (osseus) => {
-  console.log(`osseus.config.keys: ${osseus.config.keys}`)
+  const log = (msg) => {
+    if (osseus.config.debug) {
+      console.log(msg)
+    }
+  }
+  log(`osseus.config.keys: ${osseus.config.keys}`)
   osseus.config.keys.forEach(async (key) => {
-    console.log(`key: ${key}`)
+    log(`key: ${key}`)
     if (key.startsWith('osseus')) {
       let moduleName = key.substring(7) // 'osseus_' length
       let dependencies = osseus.config[key].dependencies || []
       if (typeof dependencies === 'string') {
         dependencies = dependencies.split(',')
       }
-      console.log(`${key} dependencies: ${dependencies}`)
+      log(`${key} dependencies: ${dependencies}`)
       modules[moduleName] = dependencies
       modules[moduleName].push(async () => {
         key = key.replace('_', '-')
-        console.log(`require(${key}).init()...`)
+        log(`require(${key}).init()...`)
         let module = await require(key).init(osseus)
-        console.log(`required: ${key}`)
+        log(`required: ${key}`)
         if (module.start) {
-          console.log(`${key}.start()...`)
+          log(`${key}.start()...`)
           await module.start()
-          console.log(`started ${key}`)
+          log(`started ${key}`)
         }
         osseus[moduleName] = module
         return module
